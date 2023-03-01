00da7a05e0fd4d274143acc4902d569b1d201a1a
itscarew <itscarew@gmail.com>
2023-03-01 17:36:17 +0100

    new development

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const UrlApi = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/shortenUrl`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
   headers: {
     "content-type": "application/json",
   },
